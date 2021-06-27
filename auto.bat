f17743da56a177ed2377b51877063f1e549abb70
Evan Zhang <zhang21evan@ncssm.edu>
2021-06-27 18:27:00 -0400

    weekly commit

--- auto.bat
+++ auto.bat
@@ -1,3 +1,19 @@
+cd ../
+cd ../
+cd Users
+cd evanz
+cd Desktop
+cd allbots
+cd valorant
+dir
+dir
+dir
+dir
+dir
+dir
+dir
+dir
+cd valorant
 git add .
 git commit -m "weekly commit"
 git push origin master
